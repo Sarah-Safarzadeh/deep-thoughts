34b308210307830f86ed5de01d32774478f6e558
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-06 14:05:39 -0600

    created schema files, import export typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,8 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our TypeDefs
+const TypeDefs = gql``;
+
+// export typeDefs
+module.export = TypeDefs;
