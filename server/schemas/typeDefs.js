007c38c11551223e8afae8048678cb700ce03596
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-06 20:18:18 -0600

    resolver and user thought import

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,15 @@ const { gql } = require('apollo-server-express');
 
 // create our TypeDefs
 const typeDefs = gql`
+type Thought {
+  _id: ID
+  thoughtText: String
+  createdAt: String
+  username: String
+  reactionCount: Int
+}
   type Query {
-    helloWorld: String
+    thoughts: [Thought]
   }
 `;
 
