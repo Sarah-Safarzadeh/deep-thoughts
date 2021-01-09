c172f253c78090d588d4e9c5b7b676949ed14d0c
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-09 15:45:21 -0600

    add friend mutation method

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -96,6 +96,21 @@ const resolvers = {
       }
 
       throw new AuthenticationError('You need to be logged in!');
+    },
+
+    // Add Friend
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { friends: friendId } },
+          { new: true }
+        ).populate('friends');
+    
+        return updatedUser;
+      }
+    
+      throw new AuthenticationError('You need to be logged in!');
     }
   },
 };
