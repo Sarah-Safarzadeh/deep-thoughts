8d78da8282572b92b3fafe96920237387697971c
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-12 13:52:09 -0600

    code to serve static assets, get route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
 
+// path import
+const path = require('path');
+
 // Middleware
 const { authMiddleware } = require('./utils/auth');
 
@@ -24,6 +27,17 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+// check to see if the Node environment is in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// if we make a GET request to any location on the server that doesn't have an explicit route defined, respond with the production-ready React front-end code
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
