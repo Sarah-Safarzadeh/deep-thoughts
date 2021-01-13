b064ac685898ae5cec255dc9165f6ae87d240ecb
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-13 17:59:24 -0600

    created friendlist, import, included in Profile component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 
 // React Hook
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-import ThoughtList from '../components/ThoughtList';
+import ThoughtList from "../components/ThoughtList";
 
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_USER } from '../utils/queries';
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_USER } from "../utils/queries";
+
+import FriendList from "../components/FriendList";
 
 const Profile = () => {
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(QUERY_USER, {
-    variables: { username: userParam }
+    variables: { username: userParam },
   });
 
   const user = data?.user || {};
@@ -31,7 +33,18 @@ const Profile = () => {
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
-          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+          <ThoughtList
+            thoughts={user.thoughts}
+            title={`${user.username}'s thoughts...`}
+          />
+        </div>
+
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
         </div>
       </div>
     </div>
