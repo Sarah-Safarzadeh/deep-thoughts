42658fa33c6c707e6ae9b126badfbbf0a2298784
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-01-13 18:20:01 -0600

    import gql tmplt literal, login mutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,15 @@
+// import the gql tagged template literal functionality
+import gql from 'graphql-tag';
+
+// login mutation
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
